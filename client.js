7a0f958c79307fc432e8d367b66a823a809c24b7
Olivier Rousseau-Guyot <zeachco@gmail.com>
2023-03-01 23:14:45 -0500

    fixed carrousel and based idle time off os idle

--- client.js
+++ client.js
@@ -1,7 +1,10 @@
-let idleTimeout = 0;
 let adsShowTimeout = 0;
+let currentAd;
+let previousAd;
 
 function init(config) {
+  console.table(config);
+
   const resetFormBtn = document.createElement("button");
   resetFormBtn.classList.add("reset-form");
   resetFormBtn.innerHTML = "Revenir au concours";
@@ -13,54 +16,35 @@ function init(config) {
 
   const top = document.getElementById("top");
 
-  createAds(top, config.ads_idle_items);
-
-  console.log(config);
-
-  // Idle detection
-  window.addEventListener("mousemove", resetIdleTimer);
-  window.addEventListener("mousedown", resetIdleTimer);
-  window.addEventListener("keypress", resetIdleTimer);
-  window.addEventListener("DOMMouseScroll", resetIdleTimer);
-  window.addEventListener("mousewheel", resetIdleTimer);
-  window.addEventListener("touchmove", resetIdleTimer);
-  window.addEventListener("MSPointerMove", resetIdleTimer);
-
-  function resetIdleTimer() {
-    if (top.classList.contains("active")) {
-      window.location.reload();
-      top.classList.remove("active");
-    }
-    clearTimeout(idleTimeout);
-    idleTimeout = setTimeout(turnIdle, +config.ads_idle_time_ms);
-  }
-
-  function turnIdle() {
-    console.log("turnIdle");
+  window.sleep = function goToSleep() {
+    console.log("goToSleep");
     top.classList.add("active");
     itemIndex = 0;
     showNextItem();
-  }
+  };
+
+  window.wake = function wakeUp() {
+    console.log("wakeUp");
+    top.classList.remove("active");
+    clearTimeout(adsShowTimeout);
+  };
 
   function showNextItem() {
     itemIndex++;
-    if (itemIndex >= config.ads_idle_items.length) {
+    if (itemIndex >= config.ads_items.length) {
       itemIndex = 0;
     }
 
-    document.querySelectorAll(".ad-item").forEach((item, index) => {
-      if (index === itemIndex) {
-        item.classList.add("active");
-      } else {
-        item.classList.remove("active");
-      }
-    });
+    previousAd = currentAd;
+    currentAd = new AdItem(top, config.ads_items[itemIndex]);
+
+    if (previousAd) {
+      previousAd.exit();
+    }
 
     clearTimeout(adsShowTimeout);
     adsShowTimeout = setTimeout(showNextItem, +config.ads_show_time_ms);
   }
-
-  resetIdleTimer();
 }
 
 function toggleTop(top) {
@@ -77,36 +61,106 @@ function createAds(top, items) {
     const [type, ...rawPath] = item.split(/ ?: ?/);
     const path = rawPath.join(":").trim();
 
-    switch (type.trim()) {
+    top.appendChild(adItem);
+  });
+}
+
+function createAdd() {
+  const adItem = document.createElement("div");
+  adItem.classList.add("ad-item");
+  adItem.setAttribute("data-index", index);
+  adItem.setAttribute("meta-path", item);
+
+  const [type, ...rawPath] = item.split(/ ?: ?/);
+  const path = rawPath.join(":").trim();
+
+  switch (type.trim()) {
+    case "video":
+      const video = document.createElement("video");
+      video.setAttribute("controls", "");
+      video.setAttribute("autoplay", "");
+      video.setAttribute("name", "media");
+      const source = document.createElement("source");
+      source.src = path;
+      source.type = "video/mp4";
+      video.appendChild(source);
+
+      video.src = path;
+      adItem.appendChild(video);
+      break;
+    case "image":
+    case "img":
+      const img = document.createElement("img");
+      img.src = path;
+      adItem.appendChild(img);
+      break;
+    case "site":
+    case "frame":
+      const frame = document.createElement("iframe");
+      frame.src = path;
+      frame.classList.add("ad-frame");
+      adItem.appendChild(frame);
+      break;
+    default:
+      console.log("Unknown file type");
+  }
+  top.appendChild(adItem);
+}
+
+class AdItem {
+  constructor(container, item) {
+    this.container = container;
+    const [type, ...rawPath] = item.split(/ ?: ?/);
+    this.type = type.trim();
+    this.path = rawPath.join(":").trim();
+    this.#generateHTML();
+  }
+
+  exit() {
+    this.el.classList.remove("enter");
+    this.el.classList.add("exit");
+    setTimeout(() => this.#remove(), 1000);
+  }
+
+  #remove() {
+    this.container.removeChild(this.el);
+  }
+
+  #generateHTML() {
+    this.el = document.createElement("div");
+    this.el.classList.add("ad-item");
+    switch (this.type) {
       case "video":
         const video = document.createElement("video");
         video.setAttribute("controls", "");
         video.setAttribute("autoplay", "");
         video.setAttribute("name", "media");
         const source = document.createElement("source");
-        source.src = path;
+        source.src = this.path;
         source.type = "video/mp4";
         video.appendChild(source);
-
-        video.src = path;
-        adItem.appendChild(video);
+        this.el.appendChild(video);
         break;
       case "image":
       case "img":
         const img = document.createElement("img");
-        img.src = path;
-        adItem.appendChild(img);
+        img.src = this.path;
+        this.el.appendChild(img);
         break;
       case "site":
       case "frame":
         const frame = document.createElement("iframe");
-        frame.src = path;
+        frame.src = this.path;
         frame.classList.add("ad-frame");
-        adItem.appendChild(frame);
+        this.el.appendChild(frame);
         break;
       default:
         console.log("Unknown file type");
     }
-    top.appendChild(adItem);
-  });
+    this.container.appendChild(this.el);
+
+    setTimeout(() => {
+      this.el.classList.add("enter");
+    });
+  }
 }
--- main.js
+++ main.js
@@ -1,16 +1,19 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, session, powerMonitor } = require("electron");
 const path = require("path");
 
 const devMode = process.env.NODE_ENV !== "production";
 
 const config = require("./config.json");
 let win;
+let isIdle = false;
 
 function createWindow() {
+  const currentSession = session.fromPartition("persist:someName").cookies;
   win = new BrowserWindow({
     width: 1080,
     height: 1920,
     webPreferences: {
+      session: currentSession,
       nodeIntegration: true, // enable node integration in the renderer process
     },
   });
@@ -18,13 +21,26 @@ function createWindow() {
   if (devMode) {
     win.maximize();
     win.openDevTools();
-    config.ads_idle_time_ms = 5000;
+    config.ads_idle_time_sec = 3;
     config.ads_show_time_ms = 2000;
   } else {
     win.fullScreen = true;
   }
 
   win.loadFile(path.join(__dirname, "index.html"));
+
+  setInterval(() => {
+    const idleTime = powerMonitor.getSystemIdleTime();
+    const shouldBeIdle = idleTime > config.ads_idle_time_sec;
+    if (shouldBeIdle !== isIdle) {
+      isIdle = shouldBeIdle;
+      console.log("changing idle state", isIdle);
+      win.webContents.executeJavaScript(
+        shouldBeIdle ? "sleep()" : "wake()",
+        (result) => console.log(result)
+      );
+    }
+  }, 1000);
 
   win.webContents.on("did-finish-load", async () => {
     win.webContents.executeJavaScript(
